ba7290ed0197fe960aa27dfd51095928a7e73786
elsid <elsid@yandex-team.ru>
2016-04-30 17:56:53 +0300

    Add parsers for arithmetic types

--- include/yamail/yextractor/detail/parser.hpp
+++ include/yamail/yextractor/detail/parser.hpp
@@ -3,17 +3,38 @@
 
 #include <yamail/yextractor/errors.hpp>
 
+#include <sstream>
+#include <type_traits>
+
 namespace yamail {
 namespace yextractor {
 namespace detail {
 
-template <class T>
-struct Parser;
+template <class T, class Enabled = void>
+struct Parser {};
 
 template <>
-struct Parser<std::string> {
+struct Parser<std::string, void> {
     Errors operator()(std::string& dst, const std::string& src) const {
         dst = src;
+        return Errors();
+    }
+};
+
+template <>
+struct Parser<bool, void> {
+    Errors operator()(bool& dst, const std::string& src) const {
+        std::istringstream stream(src);
+        stream >> std::boolalpha >> dst;
+        return Errors();
+    }
+};
+
+template <class T>
+struct Parser<T, typename std::enable_if<std::is_arithmetic<T>::value, void>::type> {
+    Errors operator()(T& dst, const std::string& src) const {
+        std::istringstream stream(src);
+        stream >> dst;
         return Errors();
     }
 };
--- tests/parser.cpp
+++ tests/parser.cpp
@@ -6,6 +6,7 @@ namespace {
 
 using namespace testing;
 
+using namespace yamail::yextractor;
 using namespace yamail::yextractor::detail;
 
 struct ParserTest : Test {};
@@ -16,6 +17,38 @@ TEST(ParserTest, parse_string_should_succeed) {
     const auto errors = parse(result, "string");
     EXPECT_EQ(errors, Errors());
     EXPECT_EQ(result, "string");
+}
+
+TEST(ParserTest, parse_bool_should_succeed) {
+    const Parser<bool> parse;
+    bool result;
+    const auto errors = parse(result, "true");
+    EXPECT_EQ(errors, Errors());
+    EXPECT_EQ(result, true);
+}
+
+TEST(ParserTest, parse_char_should_succeed) {
+    const Parser<char> parse;
+    char result;
+    const auto errors = parse(result, "a");
+    EXPECT_EQ(errors, Errors());
+    EXPECT_EQ(result, 'a');
+}
+
+TEST(ParserTest, parse_int_should_succeed) {
+    const Parser<int> parse;
+    int result;
+    const auto errors = parse(result, "42");
+    EXPECT_EQ(errors, Errors());
+    EXPECT_EQ(result, 42);
+}
+
+TEST(ParserTest, parse_double_should_succeed) {
+    const Parser<double> parse;
+    double result;
+    const auto errors = parse(result, "42e13");
+    EXPECT_EQ(errors, Errors());
+    EXPECT_EQ(result, 42e13);
 }
 
 } // namespace
